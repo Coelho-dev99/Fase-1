5e72e36a80dadf214244c355db53cf4314d6496b
marcio eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-07-22 16:55:56 -0300

    Create Help.py

---
+++ Help.py
@@ -0,0 +1,20 @@
+#tipos de dados#
+
+#str (utilizado para dados em texto)
+#int (Utilizado para dados de númericos inteiros)
+#float (Utilizado para dados de númericos quebrados)
+#boolean (Utilizado para true ou false)
+#type (serve para mostrar o tipo do valor) exemplo: (type () ) - tudo que tiver dentro das "" é considero como str.
+
+#Matemática#
+
+# + (simbolo para função de soma)
+# - (simbolo para função de divisão)
+# * (Simbolo para função de multiplicação)
+# / (Simbolo para função de divisão)
+# ** (Simbolo para função de potencia)
+# % (Simbolo para função de porcetagem)
+
+print ("teste") # O comando print server para mostar na tela a mensagem que está dentro ("").
+
+print (30+10) # Realiza função de soma 
