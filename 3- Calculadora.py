a1b6c9b018511a5bdcbeb41c1ef967f74fb1ddbe
marcio eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-07-23 09:32:23 -0300

    Update 3- Calculadora.py

--- 3- Calculadora.py
+++ 3- Calculadora.py
@@ -1,4 +1,25 @@
-Numero1 =int (input (" Digite o primeiro número: ") )
-Numero2 =int (input (" Digite o segundo número: ") )
+Numero1 = float (input (" Digite o primeiro número: ") )
+Numero2 = float (input (" Digite o segundo número: ") )
 
-Operacao = [soma, subtração, multiplicacao, divisao]
+operação = input ("Digite a operação desejada ( + , - , * , / , ** , % ):")
+
+if operação == "+":
+    resultado = Numero1 + Numero2
+    print ("O resultado da soma é: ", resultado)
+elif operação == "-":
+    resultado = Numero1 - Numero2
+    print ("O resultado da subtração é: ", resultado)
+elif operação == "*":
+    resultado = Numero1 * Numero2
+    print ("O resultado da multiplicação é: ", resultado)
+elif operação == "/":
+    resultado = Numero1 / Numero2
+    print ("O resultado da divisão é: ", resultado)
+elif operação == "**":
+    resultado = Numero1 ** Numero2
+    print ("O resultado da potência é: ", resultado)
+elif operação == "%":
+    resultado = Numero1 % Numero2
+    print ("O resultado do módulo é: ", resultado)
+else:
+    print ("Operação inválida!")
