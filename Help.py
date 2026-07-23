f183cab2a8edbbb6c6617217b3fcab7b66c6952f
marcio eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-07-23 09:21:08 -0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -9,7 +9,7 @@
 #Matemática#
 
 # + (simbolo para função de soma)
-# - (simbolo para função de divisão)
+# - (simbolo para função de subtração)
 # * (Simbolo para função de multiplicação)
 # / (Simbolo para função de divisão)
 # ** (Simbolo para função de potencia)
@@ -18,3 +18,7 @@
 print ("teste") # O comando print server para mostar na tela a mensagem que está dentro ("").
 
 print (30+10) # Realiza função de soma 
+print (30-10) # Realiza função de subtração
+print (30*10) # Realiza função de multiplicação
+print (30**10) # Realiza função de potência
+print (30%10) # Realiza função de sporcetagem
